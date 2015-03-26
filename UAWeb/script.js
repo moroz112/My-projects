49d45a117e3c9094187d5edaca5374c3ede231eb
moroz <moroz@gmail.com>
2015-03-26 08:20:08 +0200

    add first three questions

--- UAWeb/script.js
+++ UAWeb/script.js
@@ -3,18 +3,25 @@
  */
 $(function(){
    var questions = {
-       'question1':{
+       question1:{
            name:'Do you like JS',
            variants:{
                1: 'yes',
                2: 'no'
            }
        },
-       'question2':{
+       question2:{
            name: 'Do you like OOP',
            variants:{
                1: 'Of course',
                2: 'Hell no'
+           }
+       },
+       question3:{
+           name: 'fuck off mother fucker',
+           variants: {
+               1: 'fuck you ',
+               2: 'fuck me'
            }
        }
 
@@ -22,16 +29,55 @@ $(function(){
 
     var collection = $('.answer-variant');
     var i = 1;
-    $('.question-text').append(questions.question1.name);
     collection.each(function(){
         $(this).append(questions.question1.variants[i]);
         i++;
     });
+    var questionPlace = $('.question-text');
 
-    $('.get-answer').on('click', function(){
+    function render(event){
         var answers = $('input:checked').val();
-            if(answers == questions.question1.variants[1]){
-                console.log('it works');
-            }
-        });
+        var ans = $('input');
+        var i =1;
+        var k =1;
+        switch (answers) {
+            case questions.question1.variants[1]: questionPlace.empty();
+                questionPlace.append(event.data.question.question2.name);
+
+                ans.each(function(){
+                    $(this)[0].value = questions.question2.variants[k];
+                    k++;
+                });
+
+                collection.each(function(){
+                    $(this).empty();
+                    $(this).append(questions.question2.variants[i]);
+                    i++;
+                });
+
+                break;
+            case questions.question1.variants[2]: questionPlace.empty();
+                questionPlace.append(event.data.question.question3.name);
+                collection.each(function(){
+                    $(this).empty();
+                    $(this).append(questions.question3.variants[i]);
+                    i++;
+                });
+                break;
+            case questions.question2.variants[1]: questionPlace.empty();
+                questionPlace.append(event.data.question.question1.name);
+                collection.each(function(){
+                    $(this).empty();
+                    $(this).append(questions.question1.variants[i]);
+                    i++;
+                });
+                break;
+        }
+
+        console.log(event.data.question.question1.name)
+    };
+
+    questionPlace.append(questions.question1.name);
+
+    $('.get-answer').on('click',{question: questions},render);
 });
